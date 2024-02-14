727b911950195e49593f38a1aeae5d5b111217b7
FelipeAraujo32 <felipecafsx@gmail.com>
2024-02-14 19:08:17 -0300

    Incremento do DTO
    Realizei a adição do DTO e Validation. Incrementei métados de retorno de dados para o fórmulario

--- webregesc/src/main/java/com/web/webregesc/controllers/TeacherController.java
+++ webregesc/src/main/java/com/web/webregesc/controllers/TeacherController.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.web.webregesc.dto.NewtTeacherRequest;
 import com.web.webregesc.models.StatusTeacher;
 import com.web.webregesc.models.Teacher;
 import com.web.webregesc.repository.TeacherRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class TeacherController {
@@ -25,22 +29,26 @@ public class TeacherController {
         ModelAndView mv = new ModelAndView(("teachers/index"));
         mv.addObject("teachers", teachers);
         return mv;
-        
     }
     
-    @GetMapping("teacher/new")
+    @GetMapping("teachers/new")
     public ModelAndView nnew(){
         ModelAndView mv = new ModelAndView("teachers/new");
-        mv.addObject("statusTeacher", StatusTeacher.values());
+        mv.addObject("liststatusTeacher", StatusTeacher.values());
         return mv;
     }
 
     @PostMapping("/teachers")
-    public String create(Teacher teacher){
-        System.out.println();
-        System.out.println(teacher);
-        System.out.println();
-        return "redirect:/teachers";
+    public ModelAndView create(@Valid NewtTeacherRequest newtTeacherRequest, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            ModelAndView mv = new ModelAndView("teachers/new");
+            mv.addObject("liststatusTeacher", StatusTeacher.values());
+            return mv;
+        }
+        else{
+            Teacher teacher = newtTeacherRequest.toteacher();
+            this.teacherRepository.save(teacher);
+            return new ModelAndView("redirect:/teachers");
+        }
     }
-
 }
---
+++ webregesc/src/main/java/com/web/webregesc/dto/NewtTeacherRequest.java
@@ -0,0 +1,49 @@
+package com.web.webregesc.dto;
+
+import java.math.BigDecimal;
+
+import com.web.webregesc.models.StatusTeacher;
+import com.web.webregesc.models.Teacher;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class NewtTeacherRequest {
+    
+    @NotBlank
+    @NotNull
+    private String name;
+    @DecimalMin(value = "0.0")
+    @NotNull
+    private BigDecimal remuneration;
+    private StatusTeacher statusTeacher;
+
+    public Teacher toteacher(){
+        Teacher newTeacher = new Teacher();
+        newTeacher.setName(name);
+        newTeacher.setRemuneration(remuneration);
+        newTeacher.setStatusTeacher(statusTeacher);
+        return newTeacher;
+    }
+   
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public BigDecimal getRemuneration() {
+        return remuneration;
+    }
+    public void setRemuneration(BigDecimal remuneration) {
+        this.remuneration = remuneration;
+    }
+    public StatusTeacher getStatusTeacher() {
+        return statusTeacher;
+    }
+    public void setStatusTeacher(StatusTeacher statusTeacher) {
+        this.statusTeacher = statusTeacher;
+    }
+
+}
